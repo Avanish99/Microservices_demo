3adc829f1582ae9553e8e6beb16936b39e43e093
Avanish Yemula <avanish@Avanish-Mac-Book.local>
2025-05-14 14:24:55 -0400

    New code updated

---
+++ Microservices10/src/main/java/com/example/Microservices10/SpringCloudConfigRemoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.Microservices10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigRemoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigRemoteApplication.class, args);
+	}
+
+}
